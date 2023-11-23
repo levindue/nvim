2aae41af0862724a8cb645814fb3a832838001f9
levindue <levin@duesterhus.net>
2023-11-23 09:21:42 +0100

    it do be working

--- init.lua
+++ init.lua
@@ -5,7 +5,7 @@ require("config.keymap")
 -- plugins
 require("plugins.completion").setup({
     window = {
-        max_width = 10,
+        max_width = 25,
     },
 })
 require("plugins.statusline").setup()
--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,25 +1,54 @@
 local M = {}
 
+local va = vim.api
+local autocmd = va.nvim_create_autocmd
+
+local function calculate_width(lines, max_width_config)
+    local max_width = 0
+    for _, line in ipairs(lines) do
+        max_width = math.max(max_width, #line)
+    end
+    return math.min(max_width, max_width_config)
+end
+
+local function calculate_height(lines, max_height_config)
+    return math.min(#lines, max_height_config, vim.o.lines - 1)
+end
+
 function test(config)
-    local current_line = vim.fn.getline('.')
-    local cursor = vim.api.nvim_win_get_cursor(0)
-    local text_width = #current_line
-    local width = math.min(text_width, config.window.max_width)
+    local lines = {
+        vim.fn.getline('.'),
+    }
+
+    local cursor = va.nvim_win_get_cursor(0)
+    local width = calculate_width(lines, config.window.max_width)
+    local height = calculate_height(lines, config.window.max_height)
 
     local opts = {
         relative = "editor",
         width = width,
-        height = 1,
-        row = cursor[1] + 1,
-        col = cursor[2],
+        height = height,
+        row = cursor[1],
+        col = cursor[2] + 8,
         style = "minimal",
         border = config.window.border,
     }
 
-    local buf = vim.api.nvim_create_buf(false, true)
-    vim.api.nvim_buf_set_lines(buf, 0, -1, false, {current_line})
+    local buf = va.nvim_create_buf(false, true)
+    va.nvim_buf_set_lines(buf, 0, -1, false, lines)
 
-    local win = vim.api.nvim_open_win(buf, false, opts)
+    local win = va.nvim_open_win(buf, false, opts)
+    local win_id = win
+
+    autocmd("CursorMoved", {
+        pattern = "",
+        callback = function()
+            if va.nvim_win_is_valid(win_id) then
+                va.nvim_win_close(win_id, true)
+                test(config)
+            end
+        end
+    })
 end
 
 local function merge_config(default, user)
@@ -37,15 +66,17 @@ local function merge_config(default, user)
 end
 
 M.defaultConfig = {
+    keybind = "<leader>t",
     window = {
         border = "rounded",
         max_width = 80,
+        max_height = 10,
     },
 }
 
 function M.setup(userConfig)
     local config = merge_config(M.defaultConfig, userConfig or {})
-    vim.keymap.set("n", "<leader>t", function() test(config) end, { noremap = true, silent = true })
+    vim.keymap.set("n", config.keybind, function() test(config) end, { noremap = true, silent = true })
 end
 
 return M
